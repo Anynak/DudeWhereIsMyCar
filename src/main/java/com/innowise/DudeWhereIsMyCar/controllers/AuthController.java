26fa22b3c3655ad688aaf68a60a78c58a08f4b06
JavaМакака <anynak69@gmail.com>
2023-03-14 18:14:56 +0300

    small additions

--- src/main/java/com/innowise/DudeWhereIsMyCar/controllers/AuthController.java
+++ src/main/java/com/innowise/DudeWhereIsMyCar/controllers/AuthController.java
@@ -8,6 +8,7 @@ import com.innowise.DudeWhereIsMyCar.dto.responses.AuthResponseDTO;
 import com.innowise.DudeWhereIsMyCar.dto.responses.UserResponse;
 import com.innowise.DudeWhereIsMyCar.exceptions.AlreadyLoggedException;
 import com.innowise.DudeWhereIsMyCar.external.messagebrockers.kafka.current.service.CurrentProducer;
+import com.innowise.DudeWhereIsMyCar.models.Role;
 import com.innowise.DudeWhereIsMyCar.models.User;
 import com.innowise.DudeWhereIsMyCar.service.UserService;
 import jakarta.validation.Valid;
@@ -17,11 +18,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.List;
 
 @CrossOrigin
 @RestController
@@ -33,7 +36,6 @@ public class AuthController {
     private final UserMapper userMapper;
     private final AuthenticationManager authenticationManager;
     private final JWTGenerator jwtGenerator;
-    private final CurrentProducer kafkaProducer;
 
     @PostMapping("/v1/register")
     public ResponseEntity<UserResponse> registerUser(@RequestBody @Valid RegisterUserRequest userRequest, Principal principal) {
@@ -52,7 +54,10 @@ public class AuthController {
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
         String token = jwtGenerator.generateToken(authentication);
-        return new ResponseEntity<>(new AuthResponseDTO(token), HttpStatus.OK);
+        List<String> roles = authentication.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList();
+        AuthResponseDTO authResponseDTO = new AuthResponseDTO(token);
+        authResponseDTO.setRoles(roles);
+        return new ResponseEntity<>(authResponseDTO, HttpStatus.OK);
     }
 
 }
--- src/main/java/com/innowise/DudeWhereIsMyCar/dto/responses/AuthResponseDTO.java
+++ src/main/java/com/innowise/DudeWhereIsMyCar/dto/responses/AuthResponseDTO.java
@@ -3,11 +3,14 @@ package com.innowise.DudeWhereIsMyCar.dto.responses;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 public class AuthResponseDTO {
     private String accessToken;
     private String tokenType = "Bearer ";
+    private List<String> roles;
 
     public AuthResponseDTO(String accessToken) {
         this.accessToken = accessToken;
