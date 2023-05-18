31df328f354616708462bef82dc3094c7edb6071
JavaМакака <anynak69@gmail.com>
2023-05-18 18:25:52 +0300

    MDC impl

--- src/main/java/com/innowise/DudeWhereIsMyCar/controllers/AuthController.java
+++ src/main/java/com/innowise/DudeWhereIsMyCar/controllers/AuthController.java
@@ -12,8 +12,10 @@ import com.innowise.DudeWhereIsMyCar.service.AuthenticationService;
 import com.innowise.DudeWhereIsMyCar.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -29,30 +31,33 @@ import java.util.List;
 @Validated
 @RequiredArgsConstructor
 @RequestMapping("/auth")
+@Slf4j
 public class AuthController {
     private final UserService userService;
     private final UserMapper userMapper;
     private final AuthenticationService authenticate;
     private final JWTGenerator jwtGenerator;
-    private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
+
 
     @PostMapping("/v1/register")
     public ResponseEntity<UserResponse> registerUser(@RequestBody @Valid RegisterUserRequest userRequest, Principal principal) {
         if (principal != null) throw new AlreadyLoggedException("user " + principal.getName() + " is already logged");
         User user = userService.registerUser(userRequest);
-        logger.info("new user was created. userId = {}, login = {}", user.getUserId(), user.getLogin());
+        log.info("new user was created. userId = {}, login = {}", user.getUserId(), user.getLogin());
         UserResponse response = userMapper.toUserResponse(user);
+
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     @PostMapping("/v1/login")
-    public ResponseEntity<AuthResponseDTO> registerUser(@RequestBody @Valid LoginDTO loginDTO) {
-        logger.info("attempt to login user. login = {}", loginDTO.getLogin());
+    public ResponseEntity<AuthResponseDTO> authUser(@RequestBody @Valid LoginDTO loginDTO) {
+        MDC.put("User.login", loginDTO.getLogin());
+        log.info("attempt to login user");
         Authentication authentication = authenticate.authenticate(loginDTO.getLogin(), loginDTO.getPassword());
         String token = jwtGenerator.generateToken(authentication);
         List<String> roles = authentication.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList();
-
         AuthResponseDTO authResponseDTO = new AuthResponseDTO(token, roles);
+        MDC.clear();
         return new ResponseEntity<>(authResponseDTO, HttpStatus.OK);
     }
 
